60b92d3056f81a96e4edbdfd2474391eef66f8c0
Uros Orozel <uros.orozel@gmail.com>
2018-11-16 04:30:25 +0000

    Remove bond0.link, handled by netplan

--- files/udev-bootif.sh
+++ files/udev-bootif.sh
@@ -6,11 +6,3 @@ BOOT_MAC=${BOOT_MAC#*:}
 cat > /etc/udev/rules.d/70-bootif.rules  <<EOF
 SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="$BOOT_MAC", ATTR{type}=="1", NAME="enoboot0"
 EOF
-
-cat > /etc/systemd/network/bond0.link <<EOF
-[Match]
-Name=bond0
-
-[Link]
-MACAddress=${BOOT_MAC}
-EOF
