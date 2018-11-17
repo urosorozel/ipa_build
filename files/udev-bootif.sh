ea2db219a020923144f116a57223d39e75450544
Uros Orozel <uros.orozel@gmail.com>
2018-11-17 01:08:32 +0000

    Add secondary port to bond

--- files/udev-bootif.sh
+++ files/udev-bootif.sh
@@ -3,6 +3,8 @@
 BOOT_MAC=$(/bin/cat /proc/cmdline | egrep -o  'BOOTIF=([^ ]+)')
 BOOT_MAC=${BOOT_MAC//-/:}
 BOOT_MAC=${BOOT_MAC#*:}
+SECONDARY=$(echo "from netaddr import * ; mac=EUI('$BOOT_MAC'); mac.value = mac.value + 1; mac.dialect = mac_unix; print(mac)" | python)
 cat > /etc/udev/rules.d/70-bootif.rules  <<EOF
 SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="$BOOT_MAC", ATTR{type}=="1", NAME="enoboot0"
+SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="$SECONDARY", ATTR{type}=="1", NAME="enoboot1"
 EOF
