337beb2495983c46dd6afd87906a5928093403d5
Uros Orozel <uros.orozel@gmail.com>
2019-03-28 00:30:24 +0000

    Only remove leading zero if mac is 20chars

--- files/udev-bootif.sh
+++ files/udev-bootif.sh
@@ -2,7 +2,10 @@
 
 BOOT_MAC=$(/bin/cat /proc/cmdline | egrep -o  'BOOTIF=([^ ]+)')
 BOOT_MAC=${BOOT_MAC//-/:}
-BOOT_MAC=${BOOT_MAC#*:}
+# Remove leading 00: if mac is 20 chars long
+if [ ${#BOOT_MAC} -eq 21 ];then
+  BOOT_MAC=${BOOT_MAC#*:}
+fi
 SECONDARY=$(echo "from netaddr import * ; mac=EUI('$BOOT_MAC'); mac.value = mac.value + 1; mac.dialect = mac_unix_expanded; print(mac)" | python)
 cat > /etc/udev/rules.d/70-bootif.rules  <<EOF
 SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="$BOOT_MAC", ATTR{type}=="1", NAME="enoboot0"
