7233a099262110982147dc271ff74624c9fafe78
Uros Orozel <uros.orozel@gmail.com>
2019-03-28 01:42:42 +0000

    Remove BOOT_MAC string

--- files/udev-bootif.sh
+++ files/udev-bootif.sh
@@ -3,8 +3,10 @@
 BOOT_MAC=$(/bin/cat /proc/cmdline | egrep -o  'BOOTIF=([^ ]+)')
 BOOT_MAC=${BOOT_MAC//-/:}
 # Remove leading 00: if mac is 20 chars long
-if [ ${#BOOT_MAC} -eq 21 ];then
+if [ ${#BOOT_MAC} -eq 20 ];then
   BOOT_MAC=${BOOT_MAC#*:}
+else
+  BOOT_MAC=${BOOT_MAC#*=}
 fi
 SECONDARY=$(echo "from netaddr import * ; mac=EUI('$BOOT_MAC'); mac.value = mac.value + 1; mac.dialect = mac_unix_expanded; print(mac)" | python)
 cat > /etc/udev/rules.d/70-bootif.rules  <<EOF
